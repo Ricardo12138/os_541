aede87332453cd1bea36daec9922628697b767c9
buptxuesg <1758966306@qq.com>
2018-10-05 23:52:53 +0800

    文件系统1

---
+++ os_541/file.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class node
+{
+private:
+	char name[10];//文件名/目录名
+	bool type;// 0      /1
+	int size;//文件大小，目录名时为0
+	int state;//空闲：0/占用：1,默认为0
+	node *child;//文件名时为null
+	node *bro_node;
+	node *parent;//父节点
+public:
+	node();
+	node& operator=(const node& N);
+	//node*& operator=(const node*& N);
+	~node();
+
+	char* getName();
+	void setName(char name[]);
+
+	bool getType();
+	void setType(bool type);
+
+	int getSize();
+	void setSize(int size);
+
+	int getState();
+	void setState(int state);
+
+	node * getChild();
+	void setChild(node * child);
+
+	node * getBro_node();
+	void setBro_node(node * bro_node);
+
+	node * getParent();
+	void setParent(node * parent);
+};
---
+++ os_541/file_mangement.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"stdafx.h"
+#include"file.h"
+
+void Delete(node N);
+
+node Create();//根节点
+
+void Add(node &N);//N表示父节点
+
+void Search(node N);//遍历
+
+
+
