8d9c47bb30d727cf85b13b99662bdfe701b4af78
Ricardo12138 <754678483@qq.com>
2018-10-05 23:40:13 +0800

    version 0.1

---
+++ os_541/memory.cpp
@@ -0,0 +1,168 @@
+#include "memory.h"
+
+cPartition *head = NULL;	//连续分区的头指针	
+
+/*
+** 连续分区初始化
+** 初始化成功返回1，否则返回-1
+*/
+STATUS initialization()
+{
+	if (!(head = (cPartition*)malloc(sizeof(cPartition))))
+	{
+		return ERROR;
+	}
+	head->size = MAXSIZE;
+	head->state = FREE;
+	head->address = 0;
+	head->next = NULL;
+	return OK;
+}
+
+/*
+** 连续分区分配内存,采用最佳匹配法
+** 接收参数：请求内存的大小
+** 分配成功返回该内存块的首地址，否则返回-1
+*/
+STATUS allocation(int request)
+{
+	cPartition *p = head;
+	cPartition *best = NULL;
+	int difference = 99999;
+	while (p)
+	{
+		if (p->state == FREE && p->size >= request)
+		{
+			if (p->size - request < difference)
+			{
+				difference = p->size - request;
+				best = p;
+			}
+		}
+		p = p->next;
+	}
+
+	if (!best)
+	{
+		return ERROR;
+	}
+	else
+	{
+		cPartition *temp = (cPartition*)malloc(sizeof(cPartition));
+
+		temp->size = best->size - request;
+		temp->state = FREE;
+		temp->address = best->address + request;
+		temp->next = best->next;
+
+		best->size = request;
+		best->state = BUSY;
+		if (temp->size > 0)
+		{
+			best->next = temp;
+		}
+		return best->address;
+	}
+}
+
+/*
+** 连续分区回收内存
+** 接收参数：内存块的起始地址
+** 回收成功返回1，否则返回-1
+*/
+STATUS recycle(int address)
+{
+	cPartition *p = head;
+	cPartition *prior = p;
+
+	while (p && p->address != address)
+	{
+		prior = p;
+		p = p->next;
+	}
+
+	if (!p)
+	{
+		return ERROR;
+	}
+
+	cPartition *next = p->next;
+	//若p是头指针
+	if (p == prior)
+	{
+		prior = (cPartition*)malloc(sizeof(cPartition));
+		prior->address = 0;
+		prior->state = BUSY;
+		prior->size = 0;
+		prior->next = NULL;
+	}
+	//若p是尾指针
+	if (next == NULL)
+	{
+		next = (cPartition*)malloc(sizeof(cPartition));
+		next->state = BUSY;
+		next->size = 0;
+		next->next = NULL;
+	}
+
+	if (prior->state == BUSY && next->state == BUSY)
+	{
+		p->state = FREE;
+	}
+	else if (prior->state == BUSY && next->state == FREE)
+	{
+		p->state = FREE;
+		p->size = p->size + next->size;
+		p->next = next->next;
+		free(next);
+		next = NULL;
+	}
+	else if (prior->state == FREE && next->state == BUSY)
+	{
+		prior->size = prior->size + p->size;
+		prior->next = p->next;
+		free(p);
+		p = NULL;
+	}
+	else
+	{
+		prior->size = prior->size + p->size + next->size;
+		prior->next = next->next;
+		free(p);
+		free(next);
+		p = NULL;
+		next = NULL;
+	}
+	return OK;
+}
+
+/*
+** 内存的使用情况
+** 用链表形式返回内存的使用情况
+*/
+useCondition* show()
+{
+	cPartition *p = head;
+
+	useCondition* use = (useCondition*)malloc(sizeof(useCondition));
+	useCondition* usehead = use;
+	use->address = p->address;
+	use->size = p->size;
+	use->state = p->state;
+	use->next = NULL;
+
+	p = p->next;
+	while (p)
+	{
+		useCondition* temp = (useCondition*)malloc(sizeof(useCondition));
+		temp->address = p->address;
+		temp->size = p->size;
+		temp->state = p->state;
+		temp->next = NULL;
+
+		use->next = temp;
+		use = use->next;
+		p = p->next;
+	}
+	return usehead;
+}
---
+++ os_541/memory.h
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+
+#define BUSY 1
+#define FREE 0
+#define OK 1
+#define ERROR -1
+#define MAXSIZE 640			//内存容量为640KB,若要更改,请在这更改
+typedef int STATUS;
+
+//连续分区数据结构，基于链表
+typedef struct cPartition
+{
+	int size;				//长度
+	int state;				//状态(0:空闲；1:已分配)
+	int address;			//起始地址
+	cPartition *next;		//下一个
+}cPartition;
+
+//内存使用情况
+typedef struct useCondition
+{
+	int size;				//长度
+	int state;				//状态(0:空闲；1:已分配)
+	int address;			//起始地址
+	useCondition *next;		//下一个
+}useCondition;
+
